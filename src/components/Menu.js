ef5e55340a115ab78082f95b6b3c9ded0c294ebf
Thural Musaibov <tural.musaibov@outlook.com>
2022-12-10 23:58:42 +0400

    add menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,65 +1,58 @@
 import React, { useState, useContext } from "react";
-import { createUseStyles } from "react-jss";
 import cartImg from '../assets/cart.svg';
 import HandlerContext from "./HandlersContext";
 import styles from "../styles/menuStyles"
 
-const Cart = ({ menu: items }) => {
-	const handleCart = useContext(HandlerContext)
+const Menu = ({ menu: items }) => {
+
+	// const handleCart = useContext(HandlerContext)
 	const classes = styles();
 
 	const [display, setDisplay] = useState('none');
 
 	const toggleDisplay = () => {
-		if (display === "none") setDisplay("grid")
+		if (display === "none") setDisplay("block")
 		else setDisplay("none");
 	};
 
-	const countItems = (items) => {
-		const count = items
-			.reduce((sum, { count }) => sum + count, 0);
-		return String(count)
-	};
+	// const countItems = (items) => {
+	// 	const count = items
+	// 		.reduce((sum, { count }) => sum + count, 0);
+	// 	return String(count)
+	// };
 
-	const calcTotal = (items) => {
-		const total = items
-			.reduce((sum, { price, count }) => sum + (price * count), 0);
-		return String(Math.ceil(total * 10) / 10)
-	};
+	// const calcTotal = (items) => {
+	// 	const total = items
+	// 		.reduce((sum, { price, count }) => sum + (price * count), 0);
+	// 	return String(Math.ceil(total * 10) / 10)
+	// };
 
 	return (
 		<>
 			<div className={classes.cartBtn} onClick={toggleDisplay}>
 				<img src={cartImg} ></img>
-				<div className={classes.cartBadge}>{countItems(items)}</div>
+				{/* <div className={classes.cartBadge}>{countItems(items)}</div> */}
 			</div>
+
 			<div className={classes.cartBckg} style={{ display: display }} onClick={toggleDisplay}></div>
+
 			<div className={classes.cart} style={{ display: display }}>
-				<h3>Shopping Cart</h3>
+				<h3>Menu</h3>
+
 				<div className='items'>
-					{items.map(({ id, image, title, count, price }) => (
-						<div key={id} className='item'>
-							<div className="image"><img src={image} /></div>
-							<div className='details'>
-								<h5>{title}</h5>
-								<p>${(price * count)}</p>
-								<div className='counter'>
-									<button onClick={() => handleCart({ id, type: 'decrement', count })}>-</button>
-									<p>{count}</p>
-									<button onClick={() => handleCart({ id, type: 'increment' })}>+</button>
-								</div>
-							</div>
-						</div>
-					))}
+					<h4>Saved</h4>
+					<h4>Activity</h4>
+					<h4>Settings</h4>
 				</div>
-				<h3>total: ${calcTotal(items)}</h3>
+
+				{/* <h3>total: ${calcTotal(items)}</h3> */}
+
 				<div className='buttons'>
-					<button>checkout</button>
-					<button onClick={toggleDisplay}>close</button>
+					<button>Log out</button>
 				</div>
 			</div>
 		</>
 	);
 };
 
-export default Cart;
+export default Menu
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link } from "react-router-dom"
 import styles from "../styles/navbarStyles"
+import Menu from "./Menu"
+
+
 const NavBar = ({ children }) => {
 
 	const classes = styles();
@@ -13,6 +16,7 @@ const NavBar = ({ children }) => {
 					<Link to="/"><li>Home</li></Link>
 					<Link to="/posts"><li>Posts</li></Link>
 					<Link to="/contact"><li>Contact</li></Link>
+					<Menu />
 				</ul>
 				{children}
 			</nav>
