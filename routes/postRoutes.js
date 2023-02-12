67948d452219d307a8b1e21f44bc127037e99aa4
Thural Musaibov <tural.musaibov@outlook.com>
2023-02-13 00:04:48 +0400

    create comment delete route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,6 +8,7 @@ router.route("/:id").get(post.item)
 router.route("/:id/comments").post(post.add_comment)
 router.route("/edit/:id").post(post.edit_post)
 router.route('/delete/:id').post(post.delete_post)
+router.route('/:postID/comments/delete/:commentID').post(post.delete_comment)
 router.route('/like/:id').post(post.like_post)
 router.route('/unlike/:id').post(post.unlike_post)
 
