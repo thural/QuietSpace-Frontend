7b5baf1f37e1117dc3837acdb274994cc60f71d5
thural <tural.musaibov@gmail.com>
2024-01-11 19:22:22 +0400

    fix message positioning based on username

--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -5,14 +5,15 @@ import { useSelector } from "react-redux"
 const Message = ({ message }) => {
 
   const user = useSelector(state => state.userReducer);
-  const { id, username, text, reactions } = message;
+  const { id, sender, text, reactions } = message;
+  const senderName = sender.username;
 
-  const appliedStyle = username !== user.username ? {marginRight:"auto"} : {marginLeft:"auto", backgroundColor:'#f1f1f1'};
+  const appliedStyle = sender.username !== user.username ? {marginRight:"auto"} : {marginLeft:"auto", backgroundColor:'#f1f1f1'};
 
   const classes = styles()
   return (
     <div id={id} className={classes.message} style={appliedStyle}>
-       <div className={classes.sender}>{username}</div>
+       <div className={classes.sender}>{senderName}</div>
       <div className={classes.text}><p>{text}</p></div>
     </div>
   )
