d768a76cf4832db6b03d4f33f79c65399c93a266
thural <tural.musaibov@gmail.com>
2024-01-12 16:29:13 +0400

    create ui elements for contact search section

--- src/components/Chat/ContactContainer.jsx
+++ src/components/Chat/ContactContainer.jsx
@@ -1,12 +1,38 @@
 import Contact from "./Contact"
 import styles from "./styles/contactContainerStyles"
+import {useState} from "react";
 
 const ContactContainer = ({currentChat, setCurrentChat, chats}) => {
     const contacts = chats.map(chat => chat.users[0]);
     const classes = styles();
 
+    const [queryText, setQueryText] = useState("");
+
+    const handleInputChange = (event) => {
+        const value= event.target.value;
+        setQueryText(value);
+    }
+
+    const handleSubmit = (event) => {
+        //TODO: implement user search api
+    }
+
     return (
         <div className={classes.contacts}>
+            <div className={classes.seacrhSection}>
+                <form className={classes.searchInput} onSubmit={handleSubmit}>
+                    <input
+                        className='input'
+                        type='text'
+                        name='text'
+                        placeholder="search a user ..."
+                        maxLength="128"
+                        value={queryText}
+                        onChange={handleInputChange}
+                    />
+                    <button className={classes.submitBtn} type='submit'>search</button>
+                </form>
+            </div>
             {
                 contacts.map((contact, index) =>
                     <Contact
--- src/components/Chat/styles/contactContainerStyles.js
+++ src/components/Chat/styles/contactContainerStyles.js
@@ -6,7 +6,52 @@ const styles = createUseStyles({
     flexFlow: 'column nowrap',
     borderRight: '1px solid',
     gridColumn: '1/2',
-  }
+  },
+  searchSection: {
+    zIndex: '1'
+  },
+  searchInput: {
+    gap: '1rem',
+    color: 'black',
+    width: '100%',
+    height: '100%',
+    margin: 'auto',
+    display: 'flex',
+    padding: '1rem',
+    flexFlow: 'row nowrap',
+    boxShadow: 'rgb(0 0 0 / 50%) 0px 0px 16px -4px',
+    boxSizing: 'border-box',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    '& button': {
+      color: 'white',
+      width: 'fit-content',
+      border: '1px solid black',
+      padding: '4px 8px',
+      fontSize: '1rem',
+      fontWeight: '500',
+      borderRadius: '1rem',
+      backgroundColor: 'black'
+    },
+    '& .input': {
+      display: 'flex',
+      flexFlow: 'column nowrap',
+      gap: '0.5rem',
+    },
+    '& input': {
+      boxSizing: 'border-box',
+      width: '100%',
+      padding: '10px',
+      height: '1.8rem',
+      backgroundColor: '#e2e8f0',
+      border: '1px solid #e2e8f0',
+      borderRadius: '10px'
+    },
+    '& input:focus': {
+      outline: 'none',
+      borderColor: '#a7abb1',
+    }
+  },
 });
 
 export default styles
