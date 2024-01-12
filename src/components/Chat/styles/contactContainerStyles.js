56142beb5461b7566caf7ccacfcfdf1d87417f49
thural <tural.musaibov@gmail.com>
2024-01-12 16:52:15 +0400

    create dynamic styles for user query feature on chat page

--- src/components/Chat/styles/contactContainerStyles.js
+++ src/components/Chat/styles/contactContainerStyles.js
@@ -1,75 +1,84 @@
-import { createUseStyles } from "react-jss"
+import {createUseStyles} from "react-jss"
 
 const styles = createUseStyles({
-  contacts: {
-    display: 'flex',
-    flexFlow: 'column nowrap',
-    borderRight: '1px solid',
-    gridColumn: '1/2',
-  },
-  searchSection: {
-    zIndex: '1'
-  },
-  searchInput: {
-    gap: '1rem',
-    color: 'black',
-    width: '100%',
-    height: '100%',
-    margin: 'auto',
-    display: 'flex',
-    padding: '1rem',
-    flexFlow: 'row nowrap',
-    boxShadow: 'rgb(0 0 0 / 50%) 0px 0px 16px -4px',
-    boxSizing: 'border-box',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    '& button': {
-      color: 'white',
-      width: 'fit-content',
-      border: '1px solid black',
-      padding: '4px 8px',
-      fontSize: '1rem',
-      fontWeight: '500',
-      borderRadius: '1rem',
-      backgroundColor: 'black'
+    contacts: {
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        borderRight: '1px solid',
+        gridColumn: '1/2',
     },
-    '& .input': {
-      display: 'flex',
-      flexFlow: 'column nowrap',
-      gap: '0.5rem',
+    searchSection: {
+        zIndex: '1'
     },
-    '& input': {
-      boxSizing: 'border-box',
-      width: '100%',
-      padding: '10px',
-      height: '1.8rem',
-      backgroundColor: '#e2e8f0',
-      border: '1px solid #e2e8f0',
-      borderRadius: '10px'
+    searchInput: {
+        gap: '1rem',
+        color: 'black',
+        width: '100%',
+        height: 'fit-content',
+        margin: 'auto',
+        display: 'flex',
+        padding: '1rem',
+        flexFlow: 'row nowrap',
+        boxShadow: 'rgb(0 0 0 / 50%) 0px 0px 16px -4px',
+        boxSizing: 'border-box',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        '& button': {
+            color: 'white',
+            width: 'fit-content',
+            border: '1px solid black',
+            padding: '4px 8px',
+            fontSize: '1rem',
+            fontWeight: '500',
+            borderRadius: '1rem',
+            backgroundColor: 'black'
+        },
+        '& .input': {
+            display: 'flex',
+            flexFlow: 'column nowrap',
+            gap: '0.5rem',
+        },
+        '& input': {
+            boxSizing: 'border-box',
+            width: '100%',
+            padding: '10px',
+            height: '1.8rem',
+            backgroundColor: '#e2e8f0',
+            border: '1px solid #e2e8f0',
+            borderRadius: '10px'
+        },
+        '& input:focus': {
+            outline: 'none',
+            borderColor: '#a7abb1',
+        }
     },
-    '& input:focus': {
-      outline: 'none',
-      borderColor: '#a7abb1',
+    queryContainer: {
+        display: 'none',
+        minHeight: '10rem',
+        maxHeight: '50%',
+        padding: '4px',
+        zIndex: '3',
+        position: 'relative',
+        boxSizing: 'border-box'
+    },
+    queryItem: {
+        width: '100%',
+        border: '.2rem solid #3e3e3e',
+        margin: '4px 0',
+        boxSizing: 'border-box',
+        borderRadius: '.8rem',
+        background: 'white',
+        '& .username': {
+            margin: '0.5rem 1rem',
+            fontSize: '1.2rem',
+            fontWeight: 'bold'
+        },
+        '& .email': {
+            margin: '0.5rem 1rem',
+            fontSize: '1rem',
+            fontWeight: '300'
+        }
     }
-  },
-  queryContainer:{
-    display:'none',
-    width:'100%',
-    height:'fit-content',
-    maxHeight:'500px',
-  },
-  queryItem:{
-    width:'100%',
-    height:'1rem',
-    '& .username':{
-      fontSize:'1rem',
-      fontWeight:'bold'
-    },
-    '& .email':{
-      fontSize:'0.5rem',
-      fontWeight:'300'
-    }
-  }
 });
 
 export default styles
