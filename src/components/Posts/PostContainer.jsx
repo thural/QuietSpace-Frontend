0de8087e9a63b86fe692b2f8d0b54769830e7f7a
thural <tural.musaibov@gmail.com>
2024-03-24 20:37:52 +0400

    refactor feed page contents into shared container

--- src/components/Posts/PostContainer.jsx
+++ src/components/Posts/PostContainer.jsx
@@ -1,9 +1,18 @@
 import Post from "./Post"
 import React from "react"
-import { useSelector } from "react-redux";
+import CreatePostForm from "./CreatePostForm"
+import EditPostForm from "./EditPostForm"
 import { Container } from '@mantine/core';
+import { post } from "../../redux/formViewReducer"
+import { useDispatch, useSelector } from "react-redux"
+import styles from "./styles/postPageStyles"
 
 const PostContainer = () => {
+    const formView = useSelector(state => state.formViewReducer);
+    const user = useSelector(state => state.userReducer);
+
+    const dispatch = useDispatch();
+    const classes = styles();
 
     const posts = useSelector(state => state.postReducer);
 
@@ -15,6 +24,24 @@ const PostContainer = () => {
 
     return (
         <Container size="xs" {...styleProps}>
+            <div className={classes.posts}>
+            </div>
+
+            {
+                user.username &&
+                <button className="add-post-btn" onClick={() => dispatch(post())}>
+                    Add
+                </button>
+            }
+            {
+                user.username && formView.post &&
+                <CreatePostForm />
+            }
+            {
+                user.username && formView.edit.view &&
+                <EditPostForm />
+            }
+
             {
                 posts.map((post) => (<Post key={post["id"]} post={post} />))
             }
--- src/components/Posts/PostPage.jsx
+++ src/components/Posts/PostPage.jsx
@@ -1,45 +1,9 @@
-import React, {useEffect} from "react"
+import React from "react"
 import PostContainer from "./PostContainer"
-import CreatePostForm from "./CreatePostForm"
-import EditPostForm from "./EditPostForm"
-import styles from "./styles/postPageStyles"
-import {useDispatch, useSelector} from "react-redux"
-import {post} from "../../redux/formViewReducer"
-import {fetchPosts} from "../../api/postRequests";
-import {POST_URL} from "../../constants/ApiPath";
-import {loadPosts} from "../../redux/postReducer";
+
 
 const PostPage = () => {
-    const formView = useSelector(state => state.formViewReducer);
-    const user = useSelector(state => state.userReducer);
-
-
-    const dispatch = useDispatch();
-    const classes = styles();
-
-    return (
-        <>
-            <div className={classes.posts}>
-
-                {
-                    user.username &&
-                    <button className="add-post-btn" onClick={() => dispatch(post())}>
-                        Add
-                    </button>
-                }
-                {
-                    user.username && formView.post &&
-                    <CreatePostForm/>
-                }
-                {
-                    user.username && formView.edit.view &&
-                    <EditPostForm/>
-                }
-
-                <PostContainer />
-            </div>
-        </>
-    )
+    return <PostContainer />
 }
 
 export default PostPage
