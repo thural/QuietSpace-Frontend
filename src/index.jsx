3668cc773c7584b3ddf9d87c00383ac3e1ee8845
thural <tural.musaibov@gmail.com>
2024-03-26 12:32:07 +0400

    set global stale interval to 5 minutes for react-queries

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,11 @@ import { store } from "./redux/store"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 
-const queryCLient = new QueryClient()
+const queryCLient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 1000 * 60 * 5 } // fetched data will be refreshed after 5 minutes
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
