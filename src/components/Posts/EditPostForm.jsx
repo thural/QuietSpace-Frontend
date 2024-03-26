65485365b731daee5fc105579e1b62207957a1e3
thural <tural.musaibov@gmail.com>
2024-03-26 12:50:54 +0400

    refactor post edit fetching for react-query

--- src/components/Posts/EditPostForm.jsx
+++ src/components/Posts/EditPostForm.jsx
@@ -2,40 +2,45 @@ import React, { useState } from "react"
 import styles from "./styles/editPostStyles"
 import Overlay from "../Overlay"
 import { useDispatch, useSelector } from "react-redux"
-import { editPost } from "../../redux/postReducer"
-import { edit } from "../../redux/formViewReducer"
 import { fetchEditPost } from "../../api/postRequests"
 import { POST_URL } from "../../constants/ApiPath"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const EditPostForm = () => {
 
-  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
+
   const auth = useSelector(state => state.authReducer);
   const posts = useSelector(state => state.postReducer);
   const { edit: editView } = useSelector(state => state.formViewReducer);
 
-  const postId = editView["_id"];
+  const postId = editView["id"];
   const editedPost = posts.find(post => post.id === postId);
   const [postData, setPostData] = useState(editedPost);
 
   const handleChange = (event) => {
-    const { name, value } = event.target
-    setPostData({ ...postData, [name]: value })
+    const { name, value } = event.target;
+    setPostData({ ...postData, [name]: value });
   }
 
-    const handleEditPost = async (postData, postId) => {
-      try {
-        const response = await fetchEditPost(POST_URL, postData, auth["token"], postId);
-        if (response.ok) dispatch(editPost(postData));
-      } catch (error) {
-        console.log('error message from edit POST: ', error);
-      }
+  const editPostMutation = useMutation({
+    mutationFn: async (postData) => {
+      const response = await fetchEditPost(POST_URL, postData, auth["token"], postId);
+      return response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+      console.log("post edited was success")
+    },
+    onError: (error, variables, context) => {
+      console.log("error on editing post:", error.message)
     }
+  })
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    handleEditPost(postData, postId).then(() => console.log("post has been edited"));
-    dispatch(edit({ view: false, _id: postId }))
+    editPostMutation.mutate(postData);
+    dispatch(edit({ view: false, id: postId }));
   }
 
   const classes = styles();
@@ -45,7 +50,7 @@ const EditPostForm = () => {
       <Overlay />
       <div className={classes.post}>
         <h3>Edit post</h3>
-        <form onSubmit={handleSubmit}>
+        <form>
           <textarea
             className='text input'
             name='text'
@@ -54,7 +59,7 @@ const EditPostForm = () => {
             value={postData["text"]}
             onChange={handleChange}>
           </textarea>
-          <button className="submit-btn" type='submit'> Submit </button>
+          <button className="submit-btn" type='button' onClick={handleSubmit}> Submit </button>
         </form>
       </div>
     </>
