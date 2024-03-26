4c545da85f73aa7bd65f7a468f66e9e7a9e70054
thural <tural.musaibov@gmail.com>
2024-03-26 12:58:32 +0400

    add stale intervals for refetching posts

--- src/components/Posts/PostContainer.jsx
+++ src/components/Posts/PostContainer.jsx
@@ -3,52 +3,43 @@ import React from "react"
 import CreatePostForm from "./CreatePostForm"
 import EditPostForm from "./EditPostForm"
 import { useDispatch, useSelector } from "react-redux"
-import styles from "./styles/postPageStyles"
 import { post } from "../../redux/formViewReducer";
 import { POST_URL } from "../../constants/ApiPath"
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { fetchPosts } from "../../api/postRequests"
 
-function PostContainer () {
+function PostContainer() {
     const formView = useSelector(state => state.formViewReducer);
     const user = useSelector(state => state.userReducer);
     const auth = useSelector(state => state.authReducer);
+
+    const queryClient = useQueryClient();
+    const userData = queryClient.getQueryData(["user"]);
 
     const postsQuery = useQuery({
         queryKey: ["posts"],
         queryFn: async () => {
             const response = await fetchPosts(POST_URL, auth.token);
             return await response.json();
-        }
+        },
+        enabled: userData.userId !== null, // if userQuery could fetch the current user
+        staleTime: 1000 * 60 * 3, // keep data fresh up to 6 minutes
+        refetchInterval: 1000 * 60 * 6 // refetch data after 6 minutes on idle
     });
 
-    if(postsQuery.isLoading) return <h1>Loading</h1>;
-    if(postsQuery.isError) return <h1>{JSON.stringify(postsQuery.error)}</h1>
+    if (postsQuery.isLoading) return <h1>Loading</h1>;
+    if (postsQuery.isError) return <h1>{JSON.stringify(postsQuery.error)}</h1>
 
     const dispatch = useDispatch();
-    const classes = styles();
-
-    // const posts = useSelector(state => state.postReducer);
-
     const posts = postsQuery.data["content"];
 
     return (
         <>
-            {
-                user.username && <button onClick={() => dispatch(post())}>post</button>
-            }
-            {
-                user.username && formView.post &&
-                <CreatePostForm />
-            }
-            {
-                user.username && formView.edit.view &&
-                <EditPostForm />
-            }
-
-            {   !postsQuery.isLoading &&
-                posts.map((post) => (<Post key={post["id"]} post={post} />))
-            }
+            <button onClick={postsQuery.refetch}>refresh feed</button>
+            <button onClick={() => dispatch(post())}>post</button>
+            {formView.post && <CreatePostForm />}
+            {user.username && formView.edit.view && <EditPostForm />}
+            {!postsQuery.isLoading && posts.map((post) => (<Post key={post["id"]} post={post} />))}
         </>
 
     )
