4fa01b5365926e990dec54b399d85da348f6f650
thural <tural.musaibov@gmail.com>
2024-03-27 17:51:56 +0400

    decouple unnecesary fetch logic to specific components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,23 @@
-import { CHAT_PATH_BY_MEMBER, POST_URL, USER_PROFILE_URL } from "../constants/ApiPath";
+import { USER_PROFILE_URL } from "../constants/ApiPath";
 import { Route, Routes } from "react-router-dom";
 import styles from "../styles/appStyles";
 import ContactPage from "./Contact/ContactPage";
 import NavBar from "./Navbar/Navbar";
 import PostPage from "./Posts/PostPage";
 import ChatPage from "./Chat/ChatPage";
-import React, { useEffect, useState } from "react";
 
 import './App.css'
 
 import { fetchUser } from "../api/userRequests";
-import { useDispatch, useSelector } from 'react-redux';
-import { loadUser } from "../redux/userReducer"
-import { fetchChats } from "../api/chatRequests";
-import { loadChat } from "../redux/chatReducer";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import AuthPage from "./Auth/AuthPage";
 
 const App = () => {
-    const dispatch = useDispatch();
+    
+    const queryClient = useQueryClient();
+    const auth = queryClient.getQueryData("auth");
 
-    const auth = useSelector(state => state.authReducer);
-    const user = useSelector(state => state.userReducer);
-    const formView = useSelector(state => state.formViewReducer);
-
-    const [isChatError, setIsChatError] = useState(false);
-    const [isChatFetching, setIsChatFetching] = useState(true);
-
-    const userQuery = useQuery({
+    const { data: userData, isLoading } = useQuery({
         queryKey: ["user"],
         queryFn: async () => {
             const response = await fetchUser(USER_PROFILE_URL, auth.token);
@@ -36,58 +26,18 @@ const App = () => {
         enabled: auth["token"] != null,
     })
 
-    const handleFetchUser = async () => {
-        if (auth["token"] == null) {
-            dispatch(loadUser({}));
-            return;
-        }
-
-        if (userQuery.isFetched) {
-            dispatch(loadUser(userQuery.data));
-            console.log("user loaded: ", userResponseData);
-        }
-    }
-
-    const handleFetchChats = async () => {
-        try {
-            const response = await fetchChats(CHAT_PATH_BY_MEMBER + `/${user.id}`, auth["token"]);
-            const chatData = await response.json();
-            setIsChatFetching(false);
-            dispatch(loadChat(chatData));
-            console.log("chat data from fetch: ", chatData);
-        } catch (error) {
-            setIsChatError(true);
-            dispatch(loadChat([]));
-            console.log("error from chat fetch: ", error);
-        }
-    }
-
-    useEffect(() => {
-        handleFetchUser().then(() => console.log("user fetched"));
-    }, [auth]);
-
-    useEffect(() => {
-        handleFetchChats()
-            .then(() => console.log("chats loaded"));
-    }, [user]);
-
+    const isNull = (value) => typeof value === "object" && !value;
 
     const classes = styles();
-
     return (
         <div className={classes.app}>
-            {formView.auth ? (<AuthPage />) : (
+            {!isNull(userData.id) ? (<AuthPage />) : isLoading ? (<h1>Loading ..</h1>) : (
                 <>
                     <NavBar />
                     <Routes>
                         <Route path="/" element={<PostPage />} />
                         <Route path="/posts" element={<PostPage />} />
-                        <Route path="/chat" element={
-                            <ChatPage
-                                isChatFetching={isChatFetching}
-                                isChatError={isChatError}
-                            />
-                        } />
+                        <Route path="/chat" element={<ChatPage />} />
                         <Route path="/contact" element={<ContactPage />} />
                     </Routes>
                 </>
