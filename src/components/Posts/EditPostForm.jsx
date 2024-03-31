fe3282bacb9bf5627f093f33a11d32a3704f36a0
thural <tural.musaibov@gmail.com>
2024-03-31 14:13:01 +0400

    fix edit post form view states

--- src/components/Posts/EditPostForm.jsx
+++ src/components/Posts/EditPostForm.jsx
@@ -4,28 +4,32 @@ import Overlay from "../Overlay";
 import { fetchEditPost } from "../../api/postRequests";
 import { POST_URL } from "../../constants/ApiPath";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { authStore, viewStore } from "../../hooks/zustand";
 
 const EditPostForm = ({ postId }) => {
 
   const queryClient = useQueryClient();
-  const auth = queryClient.getQueryData("auth");
-  const posts = queryClient.getQueryData("posts");
+  const { data: viewData, setViewData } = viewStore();
+  const { data: authData } = authStore();
+  const posts = queryClient.getQueryData(["posts"]);
 
-  const editedPost = posts.find(post => post.id === postId);
-  const [postData, setPostData] = useState(editedPost);
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setPostData({ ...postData, [name]: value });
-  }
+  console.log("current post id: ", postId)
+  const editedPostData = posts.content.find(post => post.id === postId);
+  console.log("posts content", posts.content)
+
+
+  const [postData, setPostData] = useState(editedPostData);
+
 
   const editPostMutation = useMutation({
     mutationFn: async (postData) => {
-      const response = await fetchEditPost(POST_URL, postData, auth["token"], postId);
-      return response.json();
+      const response = await fetchEditPost(POST_URL, postData, authData.token, postId);
+      return response;
     },
     onSuccess: () => {
-      queryClient.invalidateQueries(["posts"]);
+      queryClient.invalidateQueries(["posts"], { exact: true });
+      setViewData({ overlay: false, editPost: false })
       console.log("post edited was success");
     },
     onError: (error, variables, context) => {
@@ -37,6 +41,11 @@ const EditPostForm = ({ postId }) => {
     event.preventDefault();
     editPostMutation.mutate(postData);
     // TODO: add logic to close form on submit
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPostData({ ...postData, [name]: value });
   }
 
   const classes = styles();
