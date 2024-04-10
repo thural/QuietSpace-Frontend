3c6d10ce605a54ce7194bb986231dcb0c9264a8b
thural <tural.musaibov@gmail.com>
2024-04-10 22:57:56 +0400

    create a custom hook to fetch random profile pictures from multiple apis

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Avatar, Box, Button, Container, Flex, Image, Input, Loader, LoadingOverlay, Tabs, Text } from "@mantine/core";
-import { PiClockClockwise, PiGear, PiImage, PiIntersect, PiNote, PiSignOut } from "react-icons/pi";
+import { Avatar, Box, Button, Container, Flex, Tabs, Text } from "@mantine/core";
+import { PiClockClockwise, PiIntersect, PiNote, PiSignOut } from "react-icons/pi";
 
 import styles from "./styles/profileContainerStyles";
+import { generatePfpUrls } from "../../utils/randomPfp";
+
 
 function ProfileContainer() {
 
     const classes = styles();
+
+    const generatedPfpUrls = generatePfpUrls(4, "beam");
 
     return (
         <Container size="600px" className={classes.container}>
@@ -14,14 +18,14 @@ function ProfileContainer() {
                 <Box className="profileName">
                     <Text fw={700}>thuralll</Text>
                 </Box>
-                <Avatar color="black" size="4.8rem" radius="10rem">T</Avatar>
+                <Avatar color="black" size="4.8rem" radius="10rem" src={generatedPfpUrls.getUrl()}>T</Avatar>
             </Flex>
 
             <Flex className={classes.followingSection}>
                 <Avatar.Group className={classes.avatarGroup}>
-                    <Avatar src="image.png" />
-                    <Avatar src="image.png" />
-                    <Avatar src="image.png" />
+                    <Avatar src={generatedPfpUrls.getUrl()} />
+                    <Avatar src={generatedPfpUrls.getUrl()} />
+                    <Avatar src={generatedPfpUrls.getUrl()} />
                     <Avatar>+5</Avatar>
                     <Text ta="center" fw="400">followers</Text>
                 </Avatar.Group>
@@ -37,15 +41,15 @@ function ProfileContainer() {
                 </Button>
             </Flex>
 
-            <Tabs color="black" defaultValue="timeline" style={{margin:'1rem 0'}}>
+            <Tabs color="black" defaultValue="timeline" style={{ margin: '1rem 0' }}>
                 <Tabs.List justify="center" grow>
-                    <Tabs.Tab value="timeline" leftSection={<PiClockClockwise size={24}/>}>
+                    <Tabs.Tab value="timeline" leftSection={<PiClockClockwise size={24} />}>
                         Timeline
                     </Tabs.Tab>
-                    <Tabs.Tab value="interests" leftSection={<PiIntersect size={24}/>}>
+                    <Tabs.Tab value="interests" leftSection={<PiIntersect size={24} />}>
                         Interests
                     </Tabs.Tab>
-                    <Tabs.Tab value="saved" leftSection={<PiNote size={24}/>}>
+                    <Tabs.Tab value="saved" leftSection={<PiNote size={24} />}>
                         Saves
                     </Tabs.Tab>
                 </Tabs.List>
