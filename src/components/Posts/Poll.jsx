aed0d4d6dba70a53d53a0f1f058ab0717eafbb04
thural <tural.musaibov@gmail.com>
2024-04-13 13:14:07 +0400

    implement dynamic poll state depending on data

--- src/components/Posts/Poll.jsx
+++ src/components/Posts/Poll.jsx
@@ -5,25 +5,41 @@ import { parseCount } from "../../utils/stringUtils";
 
 const Poll = ({ pollData }) => {
 
+    const parsedVoteCounts = parseCount(pollData.voteCount);
+
+    const getStyle = (option) => {
+        if (pollData.votedOption)
+            return option.label == pollData.votedOption ? {} : { filter: "opacity(0.3)" };
+        else return {}
+    }
+
+    const getShare = (option) => {
+        if (pollData.votedOption)
+            return option.voteShare;
+        else return 0;
+    }
+
+    const getText = (option) => {
+        if (pollData.votedOption)
+            return option.voteShare + "%";
+        else return "";
+    }
 
     const classes = styles();
-
-    const parsedVoteCounts = parseCount
-
 
     return (
         <Flex className={classes.pollContainer}>
             {
                 pollData.options.map(option => (
-                    <Flex className={classes.progressContainer} style={option.label == pollData.votedOption ? {} : { filter: "opacity(0.3)" }}>
+                    <Flex className={classes.progressContainer} style={getStyle(option)}>
                         <Text className={classes.optionDesc}>{option.label}</Text>
-                        <Progress className={classes.progress} color="rgba(0, 0, 0, 1)" size="xl" value={option.voteShare} />
-                        <Text className={classes.optionPerc}>{option.voteShare}%</Text>
+                        <Progress className={classes.progress} color="black" size="xl" value={getShare(option)} />
+                        <Text className={classes.optionPerc}>{getText(option)}</Text>
                     </Flex>
                 ))
             }
             <Flex className={classes.pollStatus}>
-                {!pollData.isEnded && <Text className="votes">{parseCount(pollData.voteCount)} votes</Text>}
+                {!pollData.isEnded && <Text className="votes">{parsedVoteCounts} votes</Text>}
                 {pollData.isEnded && <Text className="votes">poll has ended</Text>}
             </Flex>
         </Flex>
