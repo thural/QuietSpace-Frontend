a45f6d217200e6a0dacd3d13f1cb0945f470a1c6
thural <tural.musaibov@gmail.com>
2024-04-15 23:13:51 +0400

    decouple quer components

--- src/components/Chat/QueryContainer.jsx
+++ src/components/Chat/QueryContainer.jsx
@@ -4,31 +4,29 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useQueryUsers } from "../../hooks/useUserData";
 import { useCreateChat } from "../../hooks/useChatData";
 import { useChatStore } from "../../hooks/zustand";
-import { Anchor, Avatar, Box, Flex, Loader, LoadingOverlay, Text, Title } from "@mantine/core";
-import { generatePfp } from "../../utils/randomPfp";
+import { Anchor, Box, Flex, LoadingOverlay, Title } from "@mantine/core";
+import QueryItem from "./QueryItem";
+import QueryInput from "./QueryInput";
 
 const QueryContainer = () => {
 
     const queryClient = useQueryClient();
     const user = queryClient.getQueryData(["user"]);
 
-    const searchInput = React.useRef(null);
-    if (document.activeElement === searchInput.current) {
-        console.log("search is focused");
-    }
 
     const [focused, setFocused] = useState(false);
     const [queryText, setQueryText] = useState("");
     const [queryResult, setQueryResult] = useState([]);
+
 
     const { setActiveChatId } = useChatStore();
     const createChatMutation = useCreateChat(setActiveChatId);
     const makeQueryMutation = useQueryUsers(queryText, setQueryResult);
 
 
-
-    const handleUserClick = async (event, clickedUser) => {
+    const handleItemClick = async (event, clickedUser) => {
         event.preventDefault();
+        console.log("clicked user on query item: ", clickedUser);
         const createdChatRequestBody = { "userIds": [user.id, clickedUser.id] }
         createChatMutation.mutate(createdChatRequestBody);
     }
@@ -63,72 +61,35 @@ const QueryContainer = () => {
 
 
     const appliedStyle = (!focused) ? { display: 'none' } : { display: 'block' }
+    const inputProps = { handleInputFocus, handleInputBlur, handleKeyDown, queryText, handleInputChange }
     const classes = styles();
 
 
     return (
         <Box className={classes.searchContainer} >
 
-            <form className={classes.searchInput}>
-                <input ref={searchInput}
-                    onFocus={handleInputFocus}
-                    onBlur={handleInputBlur}
-                    onKeyDown={handleKeyDown}
-                    className='input'
-                    type='text'
-                    name='text'
-                    placeholder="search a user ..."
-                    maxLength="128"
-                    value={queryText}
-                    onChange={handleInputChange}
-                />
-            </form>
+            <QueryInput  {...inputProps} />
 
-            <div className={classes.resultContainer} style={appliedStyle} >
-
-
+            <Box className={classes.resultContainer} style={appliedStyle} >
                 {makeQueryMutation.isPending ?
-                    (<LoadingOverlay
-                        visible={true}
-                        overlayProps={{ radius: "sm", blur: 2 }}
-                    />) :
-
-                    (queryResult.length === 0) ? (
-                        <Flex className={classes.recentQueries}>
-                            <Title order={4}>recent</Title>
-                            <Anchor
-                                fw={400}
-                                fz="1rem"
-                                href=""
-                                target="_blank"
-                                underline="never"
-                            >
-                                clear all
-                            </Anchor>
-                        </Flex>
-                    ) :
+                    (<LoadingOverlay visible={true} overlayProps={{ radius: "sm", blur: 2 }} />) :
+                    (queryResult.length === 0) ?
                         (
-                            queryResult.map(user =>
-                                <Flex className={classes.queryCard}>
-                                    <Avatar
-                                        color="black"
-                                        size="2.5rem"
-                                        radius="10rem"
-                                        src={generatePfp("beam")}>
-                                        {user.username[0].toUpperCase()}
-                                    </Avatar>
-                                    <div key={user.id}
-                                        className={classes.queryItem}
-                                        onClick={(event) => handleUserClick(event, user)}
-                                    >
-                                        <Title order={5} className="username">{user.username}</Title>
-                                        <Text lineClamp={1} truncate="end" className="email">{user.email}</Text>
-                                    </div>
-                                </Flex>
-                            )
-                        )}
-            </div>
-
+                            <Flex className={classes.recentQueries}>
+                                <Title order={4}>recent</Title>
+                                <Anchor
+                                    fw={400}
+                                    fz="1rem"
+                                    href=""
+                                    target="_blank"
+                                    underline="never">
+                                    clear all
+                                </Anchor>
+                            </Flex>) :
+                        (queryResult.map((user, index) =>
+                            <QueryItem key={index} user={user} handleItemClick={handleItemClick} />
+                        ))}
+            </Box>
 
         </Box>
     )
