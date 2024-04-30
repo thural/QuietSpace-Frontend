4c2660c5c109010c430743289f9164f7ae66478d
Thural Musaibov <tural.musaibov@outlook.com>
2024-04-30 22:57:54 +0400

    implment docker containerization

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host:true,
+    port:5000,
+    watch:{
+      usePolling: true
+    }
+  },
   build: {
     outDir: './public'
   }
