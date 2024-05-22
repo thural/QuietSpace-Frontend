2a5e88916f9e7a97bd64c730f4ea0749acccb2e9
Thural Musaibov <tural.musaibov@outlook.com>
2024-05-22 20:56:37 +0400

    create conditional modal for searching users

--- src/components/Search/SearchContainer.jsx
+++ src/components/Search/SearchContainer.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
-import { Box, Container, Input, Loader, LoadingOverlay } from "@mantine/core";
+import {Anchor, Box, Container, Flex, Input, Loader, LoadingOverlay, Title} from "@mantine/core";
 import { PiMagnifyingGlassBold, PiMicrophone } from "react-icons/pi";
 
 import styles from "./styles/searchbarStyles";
@@ -7,11 +7,12 @@ import Post from "../Posts/Post";
 import {useQueryPosts} from "../../hooks/usePostData";
 import {generatePfp} from "../../utils/randomPfp";
 import {useQueryUsers} from "../../hooks/useUserData";
-import QueryItem from "../Chat/QueryItem";
+import UserQueryItem from "./UserQueryItem";
 
 function SearchContainer() {
 
     const queryInputRef = useRef();
+    const [focused, setFocused] = useState(false);
     const [userQueryResult, setUserQueryResult] = useState([]);
     const [postQueryResult, setPostQueryResult] = useState([]);
 
@@ -25,15 +26,24 @@ function SearchContainer() {
         fetchUserQuery.mutate(".");
     }, []);
 
+    useEffect(() => {
+        if(!userQueryResult.length) setFocused(false);
+    }, [userQueryResult])
+
     const handleInputChange = (event) => {
         event.preventDefault();
-        if(postQueryResult.length) return;
         const value = event.target.value;
-        if (value.length) fetchUserQuery.mutate(value);
-        else setUserQueryResult([]);
+        if(value.length) {
+            setFocused(true);
+            fetchUserQuery.mutate(value);
+        } else {
+            setFocused(false);
+            setUserQueryResult([]);
+        }
     }
 
     const handleKeyDown = (event) => {
+        if (event.key === 'Escape') setFocused(false);
         if (!queryInputRef.current.value.length) return;
         if (event.key === 'Enter') fetchPostQuery.mutate(queryInputRef.current.value);
     }
@@ -42,28 +52,42 @@ function SearchContainer() {
         // TODO: implement and call follow feature
     }
 
+    const handleInputFocus = (event) => {
+        if(event.target.value.length) setFocused(true);
+    }
+
+    const handleInputBlur = () => {
+        setFocused(false);
+    }
+
+    const resultAppliedStyle = focused ? { display: 'block' } : { display: 'none' };
+    const searchAppliedStyle = focused ? { boxShadow: '0 4px 8px -4px rgba(72, 72, 72, 0.3)'} : {};
     const classes = styles();
 
     return (
         <Container size="600px" className={classes.container}>
-            <Box className={classes.searchbar}>
+            <Box className={classes.searchbar} style={searchAppliedStyle}>
                 <PiMagnifyingGlassBold className={classes.searchIcon} />
                 <Input
                     variant="unstyled"
                     className={classes.searchInput}
                     placeholder="search a topic..."
                     onKeyDown={handleKeyDown}
+                    onFocus={handleInputFocus}
                     onChange={handleInputChange}
+                    onBlur={handleInputBlur}
                     ref={queryInputRef}
                 />
                 <PiMicrophone className={classes.searchIcon} />
             </Box>
-            {
-                fetchUserQuery.isPending ? <LoadingOverlay visible={true} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} /> :
-                    fetchUserQuery.isError ? <h1>{fetchPostQuery.error.message}</h1> :
-                        postQueryResult.length === 0 && userQueryResult.map((user, index) =>
-                            <QueryItem key={index} user={user} handleItemClick={handleItemClick} />)
-            }
+            <Box className={classes.resultContainer} style={resultAppliedStyle} >
+                {
+                    fetchUserQuery.isPending ? <LoadingOverlay visible={true} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} /> :
+                        fetchUserQuery.isError ? <h1>{fetchPostQuery.error.message}</h1> :
+                             userQueryResult.map((user, index) =>
+                                <UserQueryItem key={index} user={user} handleItemClick={handleItemClick} />)
+                }
+            </Box>
             {
                 fetchPostQuery.isPending ? <LoadingOverlay visible={true} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} /> :
                     fetchPostQuery.isError ? <h1>{fetchPostQuery.error.message}</h1> :
--- src/components/Search/styles/searchbarStyles.js
+++ src/components/Search/styles/searchbarStyles.js
@@ -31,6 +31,48 @@ const styles = createUseStyles({
         color: 'rgb(188, 188, 188)'
     },
 
+    resultContainer: {
+        width: '100%',
+        height: '50vh',
+        display: 'none',
+        zIndex: '3',
+        padding: '1rem',
+        boxShadow: '0 4px 8px -4px rgba(72, 72, 72, 0.3)',
+        borderRadius: "1rem 1rem 1rem 1rem",
+        borderBottom: '1px solid gainsboro',
+        boxSizing: 'border-box',
+        backgroundColor: 'rgb(250, 250, 250)'
+    },
+
+    recentQueries: {
+        width: '100%',
+        padding: '0 .5rem',
+        background: 'white',
+        boxSizing: 'border-box',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    queryCard: {
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%'
+    },
+    queryItem: {
+        width: '100%',
+        background: 'white',
+        boxSizing: 'border-box',
+        '& .username': {
+            margin: '0.25rem 0.5rem',
+        },
+        '& .email': {
+            margin: '0.25rem 0.5rem',
+            fontSize: '1rem',
+            fontWeight: '300',
+            maxWidth: '10em'
+        }
+    }
+
 })
 
 export default styles
