29d07eef3a8f855645206f9e8165f312f9d538ce
Thural Musaibov <tural.musaibov@outlook.com>
2024-10-24 19:14:01 +0400

    clean redundant dependencies, fix and update config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 import removeConsole from 'vite-plugin-remove-console'
+import postcssPresetMantine from 'postcss-preset-mantine'
+import postcssSimpleVars from 'postcss-simple-vars'
 
 export default defineConfig({
   resolve: {
@@ -13,7 +15,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     }
   },
-  plugins: [react(), removeConsole()],
+  plugins: [
+    react(),
+    removeConsole()
+  ],
   server: {
     host: true,
     port: 5000,
@@ -22,6 +27,34 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: './public'
+    outDir: './public',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'mantine': [
+            '@mantine/core',
+            '@mantine/dates',
+            '@mantine/form',
+            '@mantine/hooks',
+            '@mantine/modals',
+            '@mantine/nprogress'
+          ],
+          'react-vendor': [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'react-error-boundary'
+          ]
+        }
+      }
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssPresetMantine(),
+        postcssSimpleVars()
+      ]
+    }
   }
 })
