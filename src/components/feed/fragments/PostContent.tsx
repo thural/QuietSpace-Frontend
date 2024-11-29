8614a80d9c904b69a8ebfc6a68dac8864994a4c8
Thural Musaibov <tural.musaibov@outlook.com>
2024-11-29 15:46:35 +0400

    create and implement reusable component to display image contents

--- src/components/feed/fragments/PostContent.tsx
+++ src/components/feed/fragments/PostContent.tsx
@@ -4,19 +4,26 @@ import Conditional from "@/components/shared/Conditional";
 import Typography from "@/components/shared/Typography";
 import { ConsumerFn } from "@/types/genericTypes";
 import PollBox from "../poll/Poll";
+import PhotoDisplay from "@/components/shared/PhotoDisplay";
 
 interface PostContentProps {
     handleContentClick: ConsumerFn
     post: PostResponse
 }
 
-const PostContent: React.FC<PostContentProps> = ({ handleContentClick, post }) => (
-    <BoxStyled onClick={handleContentClick}>
-        <Typography>{post.text}</Typography>
-        <Conditional isEnabled={!!post.poll}>
-            <PollBox postId={post.id} pollData={post.poll} />
-        </Conditional>
-    </BoxStyled>
-);
+const PostContent: React.FC<PostContentProps> = ({ handleContentClick, post }) => {
+
+    return (
+        <BoxStyled onClick={handleContentClick}>
+            <Typography>{post.text}</Typography>
+            <Conditional isEnabled={!!post.photo}>
+                <PhotoDisplay photoResponse={post.photo} />
+            </Conditional>
+            <Conditional isEnabled={!!post.poll}>
+                <PollBox postId={post.id} pollData={post.poll} />
+            </Conditional>
+        </BoxStyled>
+    )
+};
 
 export default PostContent
---
+++ src/components/shared/PhotoDisplay.tsx
@@ -0,0 +1,29 @@
+import { PhotoResponse } from '@/api/schemas/inferred/photo';
+import { Image } from '@mantine/core';
+import { useEffect, useState } from 'react';
+
+interface PhotoDisplayProps {
+    photoResponse: PhotoResponse;
+}
+
+const PhotoDisplay: React.FC<PhotoDisplayProps> = ({ photoResponse }) => {
+    const [photoData, setPhotoData] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (photoResponse && photoResponse.data) {
+            setPhotoData(`data:${photoResponse.type};base64,${photoResponse.data}`);
+        }
+    }, [photoResponse]);
+
+    return (
+        <div>
+            {photoData ? (
+                <Image radius="md" src={photoData} alt={photoResponse.name} />
+            ) : (
+                <p>No photo available</p>
+            )}
+        </div>
+    );
+};
+
+export default PhotoDisplay;
