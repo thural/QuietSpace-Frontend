ce8761d2dbc8878c9c3abf0d4c90dac06cbf9d46
Thural Musaibov <tural.musaibov@outlook.com>
2024-12-05 22:11:21 +0400

    integrate theme switch to settings panel

--- src/components/navbar/menu/NavMenu.tsx
+++ src/components/navbar/menu/NavMenu.tsx
@@ -6,12 +6,25 @@ import useTheme from "@/services/hook/shared/useTheme";
 import { Switch } from "@mantine/core";
 import { PiBookmarkSimple, PiClockCounterClockwise, PiGearSix, PiMoon, PiSignOut, PiSun } from "react-icons/pi";
 import { RiMenu3Fill } from "react-icons/ri";
+import { createUseStyles } from "react-jss";
 
+
+
+const useStyles = createUseStyles({
+    themeSwitch: (isDarkMode: boolean) => ({
+        padding: '.5rem .25rem',
+        "& .mantine-Switch-track": {
+            width: "100%",
+            justifyContent: isDarkMode ? "start" : "end",
+        }
+    })
+});
 
 
 const NavMenu = () => {
 
     const { isDarkMode, setThemeMode } = useTheme();
+    const classes = useStyles(isDarkMode);
 
     const links: Array<CustomLinkProps> = [
         { to: "/saved", text: "saved", Component: <PiBookmarkSimple /> },
@@ -19,6 +32,8 @@ const NavMenu = () => {
         { to: "/settings", text: "settings", Component: <PiGearSix /> },
         { to: "/signout", text: "logout", Component: <PiSignOut /> },
     ];
+
+
 
     return (
         <ListMenu menuIcon={<RiMenu3Fill />} styleProps={{ iconSize: '1.75rem', fontSize: '1.75rem', width: "12.5rem" }}>
@@ -33,10 +48,12 @@ const NavMenu = () => {
                     />)
             }
             <Switch
+                size="xl"
                 checked={isDarkMode}
                 onChange={(event) => setThemeMode(event.currentTarget.checked)}
-                onLabel={<PiSun />}
-                offLabel={<PiMoon />}
+                onLabel={<PiSun size="1.75rem" />}
+                offLabel={<PiMoon size="1.75rem" />}
+                className={classes.themeSwitch}
             />
         </ListMenu>
     )
